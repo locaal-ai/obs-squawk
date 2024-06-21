e18e741d420d82f9120fec76edf621af8a13fc01
Roy Shilkrot <roy.shil@gmail.com>
2024-06-21 09:14:32 -0400

    chore: Update libarchive to version 3.7.4 and add bzip2 dependency

--- cmake/BuildSherpaOnnx.cmake
+++ cmake/BuildSherpaOnnx.cmake
@@ -1,25 +1,22 @@
 include(ExternalProject)
 
 # Set the repository URL and branch
-set(REPO_URL "git@github.com:k2-fsa/sherpa-onnx.git")
-set(REPO_BRANCH "master")
-
-# Set the build directory
-set(BUILD_DIR "${CMAKE_CURRENT_BINARY_DIR}/build")
+set(SHERPA_ONNX_URL "https://github.com/k2-fsa/sherpa-onnx/archive/refs/tags/v1.10.0.tar.gz")
 
 # Configure the ExternalProject
 ExternalProject_Add(
-    sherpa-onnx-build
-    GIT_REPOSITORY ${REPO_URL}
-    GIT_TAG ${REPO_BRANCH}
-    BUILD_COMMAND ${CMAKE_COMMAND} --build <BINARY_DIR> --config ${CMAKE_BUILD_TYPE}
-    CMAKE_GENERATOR ${CMAKE_GENERATOR}
-    INSTALL_COMMAND ${CMAKE_COMMAND} --install <BINARY_DIR> --config ${CMAKE_BUILD_TYPE}
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> 
-        -DSHERPA_ONNX_ENABLE_PORTAUDIO=OFF -DSHERPA_ONNX_ENABLE_PYTHON=OFF 
-        -DSHERPA_ONNX_ENABLE_TESTS=OFF -DSHERPA_ONNX_ENABLE_WEBSOCKET=OFF
-        -DSHERPA_ONNX_ENABLE_BINARY=OFF -DBUILD_SHARED_LIBS=ON
-)
+  sherpa-onnx-build
+  URL ${SHERPA_ONNX_URL}
+  BUILD_COMMAND ${CMAKE_COMMAND} --build <BINARY_DIR> --config ${CMAKE_BUILD_TYPE}
+  CMAKE_GENERATOR ${CMAKE_GENERATOR}
+  INSTALL_COMMAND ${CMAKE_COMMAND} --install <BINARY_DIR> --config ${CMAKE_BUILD_TYPE}
+  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+             -DSHERPA_ONNX_ENABLE_PORTAUDIO=OFF
+             -DSHERPA_ONNX_ENABLE_PYTHON=OFF
+             -DSHERPA_ONNX_ENABLE_TESTS=OFF
+             -DSHERPA_ONNX_ENABLE_WEBSOCKET=OFF
+             -DSHERPA_ONNX_ENABLE_BINARY=OFF
+             -DBUILD_SHARED_LIBS=ON)
 
 # Add the include directories
 ExternalProject_Get_Property(sherpa-onnx-build INSTALL_DIR)
@@ -38,21 +35,23 @@ set(SHERPA_LIBS
     ucd)
 
 foreach(lib ${SHERPA_LIBS})
-    add_library(${lib} SHARED IMPORTED)
-    set_property(TARGET ${lib} PROPERTY IMPORTED_LOCATION 
-        ${INSTALL_DIR}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}${lib}${CMAKE_SHARED_LIBRARY_SUFFIX})
-    # add implib for windows, except for onnxruntime and onnxruntime_providers_shared
-    if(WIN32)
-        set_property(TARGET ${lib} PROPERTY IMPORTED_IMPLIB
-            ${INSTALL_DIR}/lib/${CMAKE_IMPORT_LIBRARY_PREFIX}${lib}${CMAKE_IMPORT_LIBRARY_SUFFIX})
-    endif()
-    set_property(TARGET ${lib} PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${INSTALL_DIR}/include)
+  add_library(${lib} SHARED IMPORTED)
+  set_property(
+    TARGET ${lib} PROPERTY IMPORTED_LOCATION
+                           ${INSTALL_DIR}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}${lib}${CMAKE_SHARED_LIBRARY_SUFFIX})
+  # add implib for windows, except for onnxruntime and onnxruntime_providers_shared
+  if(WIN32)
+    set_property(
+      TARGET ${lib} PROPERTY IMPORTED_IMPLIB
+                             ${INSTALL_DIR}/lib/${CMAKE_IMPORT_LIBRARY_PREFIX}${lib}${CMAKE_IMPORT_LIBRARY_SUFFIX})
+  endif()
+  set_property(TARGET ${lib} PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${INSTALL_DIR}/include)
 
-    # install the dll to the project release directory: ${CMAKE_BINARY_DIR}/$<CONFIG>/obs-plugins/64bit
-    if(WIN32)
-        install(FILES ${INSTALL_DIR}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}${lib}${CMAKE_SHARED_LIBRARY_SUFFIX}
+  # install the dll to the project release directory: ${CMAKE_BINARY_DIR}/$<CONFIG>/obs-plugins/64bit
+  if(WIN32)
+    install(FILES ${INSTALL_DIR}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}${lib}${CMAKE_SHARED_LIBRARY_SUFFIX}
             DESTINATION ${CMAKE_SOURCE_DIR}/release/$<CONFIG>/obs-plugins/64bit)
-    endif()
+  endif()
 endforeach()
 
 # Add the sherpa-onnx target with all the dependencies
@@ -64,4 +63,3 @@ target_include_directories(sherpa-onnx INTERFACE ${INSTALL_DIR}/include)
 
 # Add the dependency
 add_dependencies(sherpa-onnx sherpa-onnx-build)
-
